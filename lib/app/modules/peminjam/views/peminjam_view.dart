95a09f5df1414dd9ed37acc4d35ce56606b2aaed
Geusan31 <rizky23ska@gmail.com>
2024-01-25 13:58:04 +0700

    first last day commit

--- lib/app/modules/peminjam/views/peminjam_view.dart
+++ lib/app/modules/peminjam/views/peminjam_view.dart
@@ -9,16 +9,25 @@ class PeminjamView extends GetView<PeminjamController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('PeminjamView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'PeminjamView is working',
-          style: TextStyle(fontSize: 20),
+        appBar: AppBar(
+          title: const Text('PeminjamView'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: controller.obx((state) => ListView.separated(
+              itemCount: state!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text("${state[index].book?.judul}"),
+                  subtitle: Text("Penulisan ${state[index].userId}"),
+                  trailing: ElevatedButton(
+                    child: Text("${state[index].status}"),
+                    onPressed: () {},
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return Divider();
+              },
+            )));
   }
 }
